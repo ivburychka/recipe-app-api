625a7d131fc014cbdd9793accc0d24417be29cb0
Ivan Burychka <iv.burychka@gmail.com>
2021-06-30 09:52:40 +0300

    create get list tags

---
+++ app/recipe/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tags in the database"""
+    serializer_class = serializers.TagSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+    queryset = Tag.objects.all()
+
+    def get_queryset(self):
+        """Return object for current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
